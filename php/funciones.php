a3e5fba9edf20ebf742036cdc4e31228621a7bfc
lexpin23 <mx90110787@coppel.com>
2022-02-18 13:18:19 -0700

    Function llenaCRM()
    Se crea la function llenaCRM(), crea conexión a servidor BD, obtiene los parámetros, y consulta con un SELECT la tabla donde obtendremos la información de los campos a llenar.
    
    Apartado INFORMACION DEL CLIENTE

--- php/funciones.php
+++ php/funciones.php
@@ -9,8 +9,62 @@ switch ($option){
     case 'mostrarFinesGestion':
         Captura::mostrarFinesGestion();
     break;
+
+    case 'llenaCRM':
+        InformacionCliente::llenaCRM(trim($_POST['numeroCliente']), trim($_POST['numeroTelefono']));
+    break;
 }
 
+Class InformacionCliente{
+
+    public static function llenaCRM($numCliente, $numTelefono){
+
+        $response = false;
+        $estado = 0;
+
+         //Conexion al servidor
+         $conn = conectaServer84();
+
+         //Creamos arreglos para guardar los datos, $datosCliente para tenerlos todos, y $arrayCliente será para aplicarles un encode
+         $datosCliente = array();
+         $arrayCliente = array();
+
+         //Realizar consulta
+         $sQuery = "SELECT * FROM tmp_generacion_lym WHERE num_cliente = '".$numCliente."'::BIGINT AND telefono_contactado = '".$numTelefono."'::BIGINT;";
+
+         //Guardar nuestro query en una variable y cerramos conexión con el servidor
+         $Consulta = pg_query($conn, $sQuery);
+         pg_close($conn);
+
+        if($Consulta > 0){
+
+            while($array = pg_fetch_array($Consulta)){
+
+                $response = true;
+                $estado = 1;
+
+                $datosCliente   ['nombreCliente']       = trim($array['nombre_cliente']);
+                $datosCliente   ['fechaNacimiento']     = trim($array['fechanacimiento']);
+                $datosCliente   ['sexo']                = trim($array['sexo']);
+                $datosCliente   ['estadoCivil']         = trim($array['estadocivil']);
+                $datosCliente   ['tipotelefono']        = trim($array['tipotelefono']);
+                $datosCliente   ['domicilio']           = trim($array['domicilio_act']);
+                $datosCliente   ['puntualidad']         = trim($array['puntualidad']);
+                $datosCliente   ['situacionEspecial']   = trim($array['situacionespecial']);
+                $datosCliente   ['vencido']             = trim($array['vencido']);
+
+                $arrayCliente[] = array_map('utf8_encode', $datosCliente);
+
+            }
+
+        }
+
+        $endJSON = array('estado' => $estado, 'response' => $response,'arrayCombo' => $arrayCliente);
+        echo json_encode($endJSON);
+
+
+    }
+}
 
 
 Class Captura{
@@ -28,9 +82,6 @@ Class Captura{
 
         //Realizar consulta
         $sQuery = "SELECT * FROM cat_fingestion_lym;";
-
-        $Consulta = pg_query($conn, $sQuery);
-        pg_close($conn);
 
         if($Consulta > 0){
 
@@ -56,4 +107,5 @@ Class Captura{
 
 
 }
+
 ?>
